cff54d872576ac9303e09763537f34a059aa09eb
kkobi03 <syc031312@naver.com>
2021-07-28 23:48:54 +0900

    글 페이지 썸네일 추가

--- Scripts/Signup.js
+++ Scripts/Signup.js
@@ -1,15 +1,15 @@
 const signup = document.getElementById('signup');
 signup.addEventListener('click',function(){
-
-
     var data = {
         id : $('#id').val(),
-        password : $('#password').val()
-    };
+        password : $('#password').val(),
+        age : $('age').val(),
+        gender: $('gender').val()
+        }
 
     $.ajax({
         type: 'POST',
-        url: '/api/members',
+        url: 'http://localhost:8080/api/members',
         dataType: 'json',
         contentType: 'application/json; charset=utf-8',
         data: JSON.stringify(data)
